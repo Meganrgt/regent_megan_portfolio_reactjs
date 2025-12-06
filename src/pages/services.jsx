0e3224c248fc8623434fd6aa2cd5d76f459b58b5
ProjetCEF <megregent@hotmail.fr>
2025-12-06 17:35:57 +0100

    add service component

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,9 +1,17 @@
 import H1Title from "../components/page-title";
+import ServiceBlock from "../components/service";
 
 const Services = () => {
     return (
         <div>
             <H1Title title="Mon offre de service" description="Voici les prestations sur lesquelles je peux intervenir"/>
+            <main>
+                <div className="row">
+                    <ServiceBlock icon="bi bi-brush" title="UX Design" description="L'UX Design est une discipline qui consiste à concevoir des produits (sites web, applications mobiles, logiciels, objets connectés etc.) enplaçant l'utilisateur au centre des préoccupations. L'objectif est de rendre l'expérience utilisateur la plus fluide et la plus agréable possible." />
+                    <ServiceBlock icon="bi bi-brush" title="UX Design" description="L'UX Design est une discipline qui consiste à concevoir des produits (sites web, applications mobiles, logiciels, objets connectés etc.) enplaçant l'utilisateur au centre des préoccupations. L'objectif est de rendre l'expérience utilisateur la plus fluide et la plus agréable possible." />
+                    <ServiceBlock icon="bi bi-brush" title="UX Design" description="L'UX Design est une discipline qui consiste à concevoir des produits (sites web, applications mobiles, logiciels, objets connectés etc.) enplaçant l'utilisateur au centre des préoccupations. L'objectif est de rendre l'expérience utilisateur la plus fluide et la plus agréable possible." />
+                </div>
+            </main>
         </div>
     )
 }
