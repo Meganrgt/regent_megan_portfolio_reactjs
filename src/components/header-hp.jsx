8a6bce61f86d9533e8e2df24ff8ab0df0e8981b7
ProjetCEF <megregent@hotmail.fr>
2026-01-03 20:05:58 +0100

    contact design

--- src/components/header-hp.jsx
+++ src/components/header-hp.jsx
@@ -1,6 +1,6 @@
 const HeaderHP = (props) => {
     return (
-        <><div id="headerhp">
+        <><div className="mb-5" id="headerhp">
             <h1>Bonjour je suis {props.name}</h1>
             <p className="h2">{props.job}</p>
             <button type="button" class="btn btn-danger">En savoir plus</button>
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -17,10 +17,12 @@ const Contact = () => {
                     <FormInput type="tel" id="floatingPhoneNumber" label="Vote numéro de téléphone" placeholder="0102030405" />
                     <FormInput type="text" id="floatingObject" label="Sujet" placeholder="Demande d'information" />
                     <div class="form-floating">
-                        <textarea class="form-control" placeholder="Ecrivez votre message" id="floatingTextarea"></textarea>
+                        <textarea style={{height:'300px'}}className="form-control" placeholder="Ecrivez votre message" id="floatingTextarea"></textarea>
                         <label for="floatingTextarea">Votre message</label>
                     </div>
-                    <button type="button" class="btn btn-primary" >Envoyer</button>
+                    <div className="text-center mt-3">
+                        <button type="button" className="btn btn-primary" >Envoyer</button>
+                    </div>
                 </ContainerColumn>
                 <ContainerColumn>
                     <H2Title title="Mes coordonnées" />
@@ -31,7 +33,7 @@ const Contact = () => {
                         <ContactDetails icon="bi bi-phone" details=" 10 20 30 40 50" />
                         <ContactDetails icon="bi bi-envelope-at" details=" john.doe@gmail.com" />
                     </address>
-                    <iframe title="Carte adresse Lyon" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5565.254312366655!2d4.794107988931177!3d45.77866189651516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4eb65edac5b3f%3A0xe01c47049cb2e2b9!2s40%20Rue%20Laure%20Diebold%2C%2069009%20Lyon!5e0!3m2!1sfr!2sfr!4v1767449761741!5m2!1sfr!2sfr" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe title="Carte adresse Lyon" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5565.254312366655!2d4.794107988931177!3d45.77866189651516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4eb65edac5b3f%3A0xe01c47049cb2e2b9!2s40%20Rue%20Laure%20Diebold%2C%2069009%20Lyon!5e0!3m2!1sfr!2sfr!4v1767449761741!5m2!1sfr!2sfr" width="100%" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </ContainerColumn>
             </Container>
         </div>
