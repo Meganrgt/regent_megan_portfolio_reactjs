08307e1f03b194bf2aecb0f34898207480c84ebe
ProjetCEF <megregent@hotmail.fr>
2026-01-03 12:32:57 +0100

    content home services

--- src/pages/mentions.jsx
+++ src/pages/mentions.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Accordion from "../components/accordion";
 import AccordionContainer from "../components/accordion-container";
 import H1Title from "../components/page-title";
@@ -7,9 +8,17 @@ const Mentions = () => {
         <div>
             <H1Title title="Mentions légales"/>
             <AccordionContainer>
-                <Accordion accordionTitle="Test" accordionBody="Ceci est un test" target="collapseOne" />
-                <Accordion accordionTitle="Test" accordionBody="Ceci est un test" target="collapseTwo"/>
-                <Accordion accordionTitle="Test" accordionBody="Ceci est un test" target="collapseThree" />
+                <Accordion accordionTitle="Editeur du site" accordionBodyTitle="John Doe" target="collapseOne">
+                    <H1Title title="Mon offre de service" description="Voici les prestations sur lesquelles je peux intervenir"/>
+                </Accordion>
+                <Accordion accordionTitle="Hébergeur" accordionBodyTitle="Alwaysdata" target="collapseTwo">
+                    <H1Title title="Mon offre de service" description="Voici les prestations sur lesquelles je peux intervenir"/>
+                </Accordion>
+                <Accordion accordionTitle="Crédits" accordionBodyTitle="Crédits" target="collapseThree">
+                    <p>Ce site a été déposé par John Doe, étudiant au <Link to="https://www.centre-europeen-formation.fr/landing/multi-marque/?code=1464&utm_source=google&utm_medium=cpc&utm_campaign=TOTAL-REF_PAYANT-Marque_CEF&gad_source=1&gad_campaignid=59552660&gbraid=0AAAAAD-SFizSgdbHGE5BMFMCZqWR9t99q&gclid=CjwKCAiAmePKBhAfEiwAU3Ko3EJy1URXpNqoIEV7ZSxDL0oqsNQynEoPkjjaqQkJLM9Gqxah0-VCtRoCiU0QAvD_BwE">Centre européen de formation</Link>.</p>
+                    <p>Les images utilisées sur ce site sont libres de droits et ont été obtenues sur le site <Link to="https://pixabay.com/fr/">Pixabey</Link>.</p>
+                    <p>La favicon de ce site a été fournie par <Link to="https://www.flaticon.com/">John Doe Icons erstellt von Freepik - Flaticon</Link>.</p>
+                </Accordion>
             </AccordionContainer>
         </div>
     )
--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -11,8 +11,8 @@ const Services = () => {
             <main>
                 <div className="row">
                     <ServiceBlock icon="bi bi-brush" title="UX Design" description="L'UX Design est une discipline qui consiste à concevoir des produits (sites web, applications mobiles, logiciels, objets connectés etc.) enplaçant l'utilisateur au centre des préoccupations. L'objectif est de rendre l'expérience utilisateur la plus fluide et la plus agréable possible." />
-                    <ServiceBlock icon="bi bi-brush" title="UX Design" description="L'UX Design est une discipline qui consiste à concevoir des produits (sites web, applications mobiles, logiciels, objets connectés etc.) enplaçant l'utilisateur au centre des préoccupations. L'objectif est de rendre l'expérience utilisateur la plus fluide et la plus agréable possible." />
-                    <ServiceBlock icon="bi bi-brush" title="UX Design" description="L'UX Design est une discipline qui consiste à concevoir des produits (sites web, applications mobiles, logiciels, objets connectés etc.) enplaçant l'utilisateur au centre des préoccupations. L'objectif est de rendre l'expérience utilisateur la plus fluide et la plus agréable possible." />
+                    <ServiceBlock icon="bi bi-code-slash" title="Développement web" description="Le développement de sites web consiste à créer des sites internet en utilisant des langages de programmation (HTML, CSS, Javascript, PHP etc.) et des frameworks (Bootstrap, React, Angular etc.)." />
+                    <ServiceBlock icon="bi bi-search" title="Référencement" description="Le référencement naturel (SEO) est une technique qui consiste à optimiser un site web pour le faire remonter dans les résultats des moteurs de recherche (Google, Bing, Yahoo etc.). L'objectif est d'attirer un maximum de visiteurs qualifiés sur ce site." />
                 </div>
             </main>
         </div>
