ff7bcac9321148d3203d5b5fe07533d862e4ef4e
ProjetCEF <megregent@hotmail.fr>
2026-01-03 20:14:35 +0100

    mentions design

--- src/components/accordion-container.jsx
+++ src/components/accordion-container.jsx
@@ -1,8 +1,8 @@
 const AccordionContainer = ({children}) => {
     return (
-        <><div className="accordion">
+        <div className="accordion">
             {children}
-            </div></>
+        </div>
     );
 }
 
