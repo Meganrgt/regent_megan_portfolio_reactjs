7b523f1d084c41c5180360502a5505673cb177ad
ProjetCEF <megregent@hotmail.fr>
2026-01-03 13:59:07 +0100

    resolve bug

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,6 +2,7 @@ import H2Title from "../components/title-h2";
 import H1Title from "../components/page-title";
 import Container from "../components/container";
 import ContainerColumn from "../components/container-column";
+import FormInput from "../components/input";
 
 const Contact = () => {
     return (
@@ -10,6 +11,15 @@ const Contact = () => {
             <Container>
                 <ContainerColumn>
                     <H2Title title="Formulaire de contact" />
+                    <FormInput type="text" id="floatingName" label="Votre nom" placeholder="John Doe"/>
+                    <FormInput type="email" id="floatingEmail" label="Vote adresse email" placeholder="example@gmail.com" />
+                    <FormInput type="tel" id="floatingPhoneNumber" label="Vote numéro de téléphone" placeholder="0102030405" />
+                    <FormInput type="text" id="floatingObject" label="Sujet" placeholder="Demande d'information" />
+                    <div class="form-floating">
+                        <textarea class="form-control" placeholder="Ecrivez votre message" id="floatingTextarea"></textarea>
+                        <label for="floatingTextarea">Votre message</label>
+                    </div>
+                    <button type="button" class="btn btn-primary" >Envoyer</button>
                 </ContainerColumn>
                 <ContainerColumn>
                     <H2Title title="Mes coordonnées" />
