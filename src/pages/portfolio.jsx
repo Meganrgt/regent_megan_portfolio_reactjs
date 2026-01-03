061b656d4d670a4963f2f4c443c9c764d7358dde
ProjetCEF <megregent@hotmail.fr>
2026-01-03 14:49:29 +0100

    add card component

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,9 +1,13 @@
+import Card from "../components/card";
 import H1Title from "../components/page-title";
+import ImageFreshFood from "../assets/images/portfolio/fresh-food.jpg";
+
 
 const Portfolio = () => {
     return (
         <div>
             <H1Title title="Portfolio" description="Voici quelques-unes de mes réalisations"/>
+            <Card imageLink={ImageFreshFood} cardTitle="Fresh Food" cardText="Site de vente de produit frais en ligne" cardFooter="site réalisé avec PHP et MySQL" />
         </div>
     )
 }
